325f35a90860bddca5ea1236e349d14a9e0b58df
Mersad Huskic <mersad.huskic@virginpulse.com>
2025-02-20 08:23:13 -0500

    Added changes to index page

--- pages/Home/index.vue
+++ pages/Home/index.vue
@@ -1,14 +1,19 @@
 <script setup>
 import {ref, reactive} from 'vue'
 import Headers from "../../components/Headers"
-
-const components = {
-  Headers
-}
-
+import Dashboard from "~/components/DashboardPage.vue";
+  definePageMeta({
+    layout: "default",
+  });
+  definePageMeta({
+    middleware: "auth",
+  })
 </script>
 
 <template>
-  <Headers/>
-  <h1>Home...</h1>
+  <div>
+    <nuxt-route-announcer />
+    <Dashboard />
+  </div>
 </template>
+
